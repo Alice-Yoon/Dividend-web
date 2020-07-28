ce7502b42cf49125443d9a540d4ac9db2077adc8
윤지혜 <yoonjihye@yunjihyeui-MacBook-Pro.local>
2020-07-28 20:04:51 +0900

    initial setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/views/pages/Home/Home';
+import Portfolio from './components/views/pages/Portfolio/Portfolio';
+import Community from './components/views/pages/Community/Community';
+import MyPage from './components/views/pages/MyPage/MyPage';
+import NotFound from './components/views/pages/NotFound/NotFound';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/community" component={Community} />
+        <Route exact path="/mypage" component={MyPage} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+    </>
     </div>
   );
 }
